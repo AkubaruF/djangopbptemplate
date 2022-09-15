afd9b56539b4797025151141ecfe01bb8551634f
akbar.firdaus <akbar.firdaus@ui.ac.id>
2022-09-15 15:49:39 +0700

    Added show_xml_by_id

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -7,6 +7,8 @@ from wishlist.views import show_json
 
 from wishlist.views import show_json_by_id
 
+from wishlist.views import show_xml_by_id
+
 app_name = 'wishlist'
 
 urlpatterns = [
@@ -14,4 +16,5 @@ urlpatterns = [
     path('xml/', show_xml, name = 'show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'), 
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
 ]
